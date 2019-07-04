416b965aa2b5da66c1986198bcd9b9c83313bcc3
longshihui <391424924@qq.com>
2019-07-04 15:40:48 +0800

    feat(ipc): 新增ipc模块，管理render进程里使用底层系统api时候的代码

---
+++ src/modules/ipc/create-pr.ts
@@ -0,0 +1,34 @@
+import { dialog } from 'electron';
+
+const RENDER_NAME = 'create-pr';
+
+export const IPCEvents = {
+  SELECT_DIRECTORY: `${RENDER_NAME}/selectDirectory`
+};
+
+export default {
+  renderName: RENDER_NAME,
+  handles: [
+    {
+      event: IPCEvents.SELECT_DIRECTORY,
+      mainProcessHandle: async event => {
+        const selectedDirectory = await new Promise(function(resolve) {
+          dialog.showOpenDialog(
+            {
+              properties: ['openDirectory'],
+              buttonLabel: '选择文件夹'
+            },
+            function(filePaths) {
+              if (Array.isArray(filePaths) && filePaths.length > 0) {
+                resolve(filePaths[0]);
+              } else {
+                resolve(null);
+              }
+            }
+          );
+        });
+        event.sender.send(IPCEvents.SELECT_DIRECTORY, selectedDirectory);
+      }
+    }
+  ]
+};
--- src/pages/create-pr/CreatePR.tsx
+++ src/pages/create-pr/CreatePR.tsx
@@ -1,10 +1,100 @@
 import React from 'react';
 import Layout from '@/components/layout';
+import { TextField, Fab, Grid } from '@material-ui/core';
+import { Folder } from '@material-ui/icons';
+import { createStyles, WithStyles, withStyles } from '@material-ui/styles';
+import { ipcRenderer } from 'electron';
+import { isString } from 'lodash';
+import { IPCEvents } from '@/modules/ipc/create-pr';
 
-export default function CreatePR() {
-  return (
-    <Layout>
-      <h1>DevelopTools!</h1>
-    </Layout>
-  );
+const styles = createStyles({
+  row: {
+    marginTop: 20,
+    marginBottom: 20
+  },
+  input: {
+    marginRight: 20,
+    flexGrow: 1,
+    flexBasis: 'auto',
+    flexShrink: 1
+  },
+  iconButton: {
+    flexGrow: 0,
+    flexBasis: 'auto',
+    flexShrink: 0
+  }
+});
+
+interface CreatePRProps extends WithStyles<typeof styles> {}
+
+interface CreatePRState {
+  selectedDirectory: string;
 }
+
+class CreatePR extends React.Component<CreatePRProps, CreatePRState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDirectory: ''
+    };
+  }
+  async selectDirectory() {
+    ipcRenderer.once(IPCEvents.SELECT_DIRECTORY, (event, directoryPath) => {
+      if (!isString(directoryPath)) {
+        return;
+      }
+      this.setState(() => {
+        return {
+          selectedDirectory: directoryPath
+        };
+      });
+    });
+    ipcRenderer.send(IPCEvents.SELECT_DIRECTORY);
+  }
+  render() {
+    const { selectedDirectory } = this.state;
+    const props = this.props;
+    return (
+      <Layout>
+        <Grid container alignItems="center" className={props.classes.row}>
+          <Grid item className={props.classes.input}>
+            <TextField
+              label="需求存放目录"
+              placeholder="选择一个路径"
+              fullWidth
+              variant="outlined"
+              value={selectedDirectory}
+              InputLabelProps={{
+                shrink: true
+              }}
+              InputProps={{
+                readOnly: true
+              }}
+            />
+          </Grid>
+          <Grid item className={props.classes.iconButton}>
+            <Fab href="" color="primary" onClick={() => this.selectDirectory()}>
+              <Folder />
+            </Fab>
+          </Grid>
+        </Grid>
+        <Grid container alignItems="center" className={props.classes.row}>
+          <Grid item className={props.classes.input}>
+            <TextField
+              label="新需求名字"
+              placeholder="请输入需求名字"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Layout>
+    );
+  }
+}
+
+export default withStyles(styles)(CreatePR);
