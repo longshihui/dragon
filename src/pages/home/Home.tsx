6927319b5d2fc78740bc33ccaa7189c79ba9241f
longshihui <391424924@qq.com>
2019-08-30 12:34:08 +0800

    refactor: 调整代码结构

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,53 +1,50 @@
 import React from 'react';
 import {
-  Grid,
-  Card,
-  CardActionArea,
-  CardContent,
-  Typography
+    Grid,
+    Card,
+    CardActionArea,
+    CardContent,
+    Typography
 } from '@material-ui/core';
 import { RouteComponentProps } from 'react-router-dom';
 import classes from './Home.scss';
 import CardIcon from './CardIcon';
 import config from '@/AppConfig';
-import { Layout } from '@/components';
 
 export default class Home extends React.Component<RouteComponentProps> {
-  constructor(props: RouteComponentProps) {
-    super(props);
-  }
-  private goTo(pathname: string) {
-    this.props.history.push(pathname);
-  }
-  render() {
-    return (
-      <Layout>
-        <Grid className={classes.content} container spacing={3}>
-          {config.map(c => {
-            return (
-              <Grid item xs={4} key={c.id}>
-                <Card onClick={() => this.goTo(c.path)}>
-                  <CardActionArea href="javascript:;">
-                    <CardContent>
-                      <CardIcon>{c.icon}</CardIcon>
-                      <Typography component="h2" variant="h5">
-                        {c.title}
-                      </Typography>
-                      <Typography
-                        component="p"
-                        variant="body2"
-                        color="textSecondary"
-                      >
-                        {c.description}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Layout>
-    );
-  }
+    constructor(props: RouteComponentProps) {
+        super(props);
+    }
+    private goTo(pathname: string) {
+        this.props.history.push(pathname);
+    }
+    render() {
+        return (
+            <Grid className={classes.content} container spacing={3}>
+                {config.map(c => {
+                    return (
+                        <Grid item xs={4} key={c.id}>
+                            <Card onClick={() => this.goTo(c.path)}>
+                                <CardActionArea href="javascript:;">
+                                    <CardContent>
+                                        <CardIcon>{c.icon}</CardIcon>
+                                        <Typography component="h2" variant="h5">
+                                            {c.title}
+                                        </Typography>
+                                        <Typography
+                                            component="p"
+                                            variant="body2"
+                                            color="textSecondary"
+                                        >
+                                            {c.description}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        );
+    }
 }
---
+++ src/theme/palettes.ts
@@ -0,0 +1,14 @@
+export default [
+    //夜晚主题色
+    {
+        time: '00:00',
+        palette: {
+            primary: {
+                main: '#304fff'
+            },
+            secondary: {
+                main: '#2962ff'
+            }
+        }
+    }
+];
