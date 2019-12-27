2e3a8ccdfcead68562375fdc4c04851f28e3b92f
longshihui <391424924@qq.com>
2019-12-27 14:13:43 +0800

    refactor(crate-pr): 兼容新的electron版本

--- src/modules/ipc/create-pr.ts
+++ src/modules/ipc/create-pr.ts
@@ -12,23 +12,18 @@ export default {
         {
             event: IPCEvents.SELECT_DIRECTORY,
             mainProcessHandle: async event => {
-                const selectedDirectory = await new Promise(function(resolve) {
-                    dialog.showOpenDialog(
-                        {
-                            properties: ['openDirectory'],
-                            buttonLabel: '选择文件夹'
-                        },
-                        function(filePaths) {
-                            if (
-                                Array.isArray(filePaths) &&
-                                filePaths.length > 0
-                            ) {
-                                resolve(filePaths[0]);
-                            } else {
-                                resolve(null);
-                            }
-                        }
-                    );
+                const selectedDirectory = await new Promise(async function(
+                    resolve
+                ) {
+                    const filePaths = await dialog.showOpenDialog(null, {
+                        properties: ['openDirectory'],
+                        buttonLabel: '选择文件夹'
+                    });
+                    if (Array.isArray(filePaths) && filePaths.length > 0) {
+                        resolve(filePaths[0]);
+                    } else {
+                        resolve(null);
+                    }
                 });
                 event.sender.send(
                     IPCEvents.SELECT_DIRECTORY,
