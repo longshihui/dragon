01030d382183ec242d46d4aa7050483b2f984e1f
longshihui <391424924@qq.com>
2020-04-04 21:14:43 +0800

    feat(deps): 更新所有包的版本

--- build/webpack.config.renderer.dev.dll.ts
+++ build/webpack.config.renderer.dev.dll.ts
@@ -4,194 +4,194 @@
  * Builds the DLL for development electron renderer process
  */
 
-import webpack from "webpack";
-import path from "path";
-import merge from "webpack-merge";
-import baseConfig from "./webpack.config.base";
-import { dependencies } from "../package.json";
+import webpack from 'webpack';
+import path from 'path';
+import merge from 'webpack-merge';
+import baseConfig from './webpack.config.base';
+import { dependencies } from '../package.json';
 
 interface Options {
-  outputPath: string;
+    outputPath: string;
 }
 
-export default function(options: Options) {
-  return merge.smart(baseConfig, {
-    entry: {
-      renderer: Object.keys(dependencies || {})
-    },
+export default function (options: Options) {
+    return merge.smart(baseConfig, {
+        entry: {
+            renderer: Object.keys(dependencies || {})
+        },
 
-    output: {
-      library: "renderer",
-      path: options.outputPath,
-      filename: "[name].dev.dll.js",
-      libraryTarget: "var"
-    },
+        output: {
+            library: 'renderer',
+            path: options.outputPath,
+            filename: '[name].dev.dll.js',
+            libraryTarget: 'var'
+        },
 
-    devtool: "eval",
+        devtool: 'eval',
 
-    mode: "development",
+        mode: 'development',
 
-    target: "electron-renderer",
+        target: 'electron-renderer',
 
-    externals: ["fsevents", "crypto-browserify"],
+        externals: ['fsevents', 'crypto-browserify'],
 
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              cacheDirectory: true
-            }
-          }
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true
+                        }
+                    }
+                },
+                {
+                    test: /\.global\.css$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /^((?!\.global).)*\.css$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                sourceMap: true,
+                                importLoaders: 1
+                            }
+                        }
+                    ]
+                },
+                // SASS support - compile all .global.scss files and pipe it to style.css
+                {
+                    test: /\.global\.(scss|sass)$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                },
+                // SASS support - compile all other .scss files and pipe it to style.css
+                {
+                    test: /^((?!\.global).)*\.(scss|sass)$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                sourceMap: true,
+                                constLoaders: 1,
+                                localIdentName:
+                                    '[name]__[local]__[hash:base64:5]'
+                            }
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                },
+                // WOFF Font
+                {
+                    test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                    use: {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            mimetype: 'application/font-woff'
+                        }
+                    }
+                },
+                // WOFF2 Font
+                {
+                    test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                    use: {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            mimetype: 'application/font-woff'
+                        }
+                    }
+                },
+                // TTF Font
+                {
+                    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                    use: {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            mimetype: 'application/octet-stream'
+                        }
+                    }
+                },
+                // EOT Font
+                {
+                    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                    use: 'file-loader'
+                },
+                // SVG Font
+                {
+                    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                    use: {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            mimetype: 'image/svg+xml'
+                        }
+                    }
+                },
+                // Common Image Formats
+                {
+                    test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
+                    use: 'url-loader'
+                }
+            ]
         },
-        {
-          test: /\.global\.css$/,
-          use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        },
-        {
-          test: /^((?!\.global).)*\.css$/,
-          use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader",
-              options: {
-                modules: true,
-                sourceMap: true,
-                constLoaders: 1,
-                localIdentName: "[name]__[local]__[hash:base64:5]"
-              }
-            }
-          ]
-        },
-        // SASS support - compile all .global.scss files and pipe it to style.css
-        {
-          test: /\.global\.(scss|sass)$/,
-          use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: "sass-loader"
-            }
-          ]
-        },
-        // SASS support - compile all other .scss files and pipe it to style.css
-        {
-          test: /^((?!\.global).)*\.(scss|sass)$/,
-          use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader",
-              options: {
-                modules: true,
-                sourceMap: true,
-                constLoaders: 1,
-                localIdentName: "[name]__[local]__[hash:base64:5]"
-              }
-            },
-            {
-              loader: "sass-loader"
-            }
-          ]
-        },
-        // WOFF Font
-        {
-          test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
-              mimetype: "application/font-woff"
-            }
-          }
-        },
-        // WOFF2 Font
-        {
-          test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
-              mimetype: "application/font-woff"
-            }
-          }
-        },
-        // TTF Font
-        {
-          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
-              mimetype: "application/octet-stream"
-            }
-          }
-        },
-        // EOT Font
-        {
-          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-          use: "file-loader"
-        },
-        // SVG Font
-        {
-          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
-              mimetype: "image/svg+xml"
-            }
-          }
-        },
-        // Common Image Formats
-        {
-          test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
-          use: "url-loader"
-        }
-      ]
-    },
 
-    plugins: [
-      new webpack.DllPlugin({
-        path: path.join(options.outputPath, "[name].json"),
-        name: "[name]"
-      }),
+        plugins: [
+            new webpack.DllPlugin({
+                path: path.join(options.outputPath, '[name].json'),
+                name: '[name]'
+            }),
 
-      /**
-       * Create global constants which can be configured at compile time.
-       *
-       * Useful for allowing different behaviour between development builds and
-       * release builds
-       *
-       * NODE_ENV should be production so that modules do not perform certain
-       * development checks
-       */
-      new webpack.EnvironmentPlugin({
-        NODE_ENV: "development"
-      })
-    ]
-  });
+            /**
+             * Create global constants which can be configured at compile time.
+             *
+             * Useful for allowing different behaviour between development builds and
+             * release builds
+             *
+             * NODE_ENV should be production so that modules do not perform certain
+             * development checks
+             */
+            new webpack.EnvironmentPlugin({
+                NODE_ENV: 'development'
+            })
+        ]
+    });
 }
